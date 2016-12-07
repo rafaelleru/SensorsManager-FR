e7c6815db70d4f1417f60f990ab3706ab8dc5fb3
rafa <rafaelleru@localhost.localdomain>
2016-12-07 12:12:08 +0100

    EEEEEEEEEEEEE

--- ej4/DataManager.java
+++ ej4/DataManager.java
@@ -3,13 +3,34 @@ import java.net.*;
 import java.util.Scanner;
 
 public class DataManager{
-    private Socket socket = null;
-    private int port;
+    //Socket UDP para el envio de peticiones al servidor
+	
+    // Nombre del host donde se ejecuta el servidor:
+    String host="localhost";
+    // Puerto en el que espera el servidor:
+    static int port=8888;
+    Data informacion = new Data();
+    static InetAddress direccion = null;
+    static DatagramSocket socket = null;
+    
+
     private static Scanner scanner = new Scanner(System.in);
     
     public DataManager(){
 	this.port = 8888;
 	//this.scanner = new Scanner(System.in);
+
+	try{
+	    socket = new DatagramSocket();
+	} catch(IOException e){
+	    System.err.println("Error creando el socket");
+	}
+
+	try{
+	    direccion = InetAddress.getByName(host);
+	} catch(IOException e){
+	    System.err.println("Error obteniendo la direccion");
+	}
     }
     
     public static void printMenu(){
@@ -19,6 +40,8 @@ public class DataManager{
 	System.out.println("[ 2 ] Consultar todas las mediciones en una determinada fecha.");
 	System.out.println("[ 3 ] Descargar una copia del fichero de mediciones de una determinada localizacion.");
 	System.out.println("[ -1 ] Exit");
+	String option = scanner.next();
+	executeOption(option);
     }
 
     public static void executeOption(String option){
@@ -26,19 +49,70 @@ public class DataManager{
 	if(opt == 1){
 	    System.out.println("Que localizacion quieres consultar asqueroso: ");
 	    String location = scanner.next();
-	    System.out.println(" ok mankina " +  location);
+	
+	    //    try{
+
+	    //Enivar peticion al servidor
+	    //} catch(IOException e){
+	    //	System.err.println("Error en la comunicacion con el servidor: " + e);
+	    //}
+	    //Lanzar metodo que recoja los datos.
+	    //try{
+	    byte[] buferEnvio;
+		buferEnvio = ("data_" + location).getBytes();
+		    DatagramPacket peticion = null;
+		peticion = new DatagramPacket(buferEnvio, buferEnvio.length, direccion, port);
+		getAndPrintData();
+		/*} catch(IOException e){
+		System.err.println("Error haciendo la peticion al servidor");
+	    }*/
 	}
+	    /*
+	if(option == 2){
+	    System.out.println("Que localizacion quieres consultar asqueroso: ");
+	    String location = scanner.next();
 
-	//if(option == 2){}
+	    try{
+		//Enivar peticion al servidor
+	    } catch(IOException e)
+		System.err.println("Error en la comunicacion con el servidor: " + e);
+	    //Lanzar metodo que recoja los datos.
+	    getAndPrintData();
+	}
+	   */ 
 
-	//if(option == 3){}
-
+	//	if(option == 3){}
+	//	if(option == -1)
+	//        return 0;
     }
 
+    public static void getAndPrintData(){
+	int port = 8888;
+	InetAddress addres = null;
+
+	try{
+	    DatagramSocket datasocket = new DatagramSocket(port);
+	    DatagramPacket datapacket = null;
+
+	do{
+	    byte[] buffer = new byte[256];
+	    datapacket = new DatagramPacket(buffer, buffer.length);
+	    datasocket.receive(datapacket);
+
+	    //Imprimir los datos
+	    System.out.println(datapacket.getData());
+	    System.out.println("********************");
+	}while(true);
+
+	} catch(IOException e){
+	    System.err.println("Error recibiendo los datos" + e);
+	}
+    }
+	    
     public static void main(String[] args){
 	printMenu();
-	String option = scanner.next();
-	executeOption(option);
+
+	//executeOption(option);
 	
     }
 }
